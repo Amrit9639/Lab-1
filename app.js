f8103cbc70abd0d0ad029b4dc8fbcb82a51ce846
Amritpal Singh <748914@student.tor.loc>
2020-01-22 16:57:14 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+var myLogModule = require('./Log.js');
+myLogModule.info('Node.js started');
+
+var msg = require('./Messages.js');
+console.log(msg);
+
+var msg = require('./Log.js');
+msg.log('Hello World');
+
+var person = require('./data.js');
+console.log(person.firstName + ' ' + person.lastName);
+
+var person = require('./Person.js');
+var person1 = new person('James', 'Bond');
+console.log(person1.fullName());
